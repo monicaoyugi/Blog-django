33dc127ac846f545c97abafee804740130571cfb
monicaoyugi <monicaoyugi@gmail.com>
2020-05-07 22:48:38 +0300

    create admin login

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
+posts = [
+    {
+        "author": "Monica",
+        "title": "Blog Post 1",
+        "content": "First post",
+        "date_posted": "May 07, 202",
+    },
+    {
+        "author": "Kisinga",
+        "title": "Blog Post 2",
+        "content": "First post",
+        "date_posted": "May 07, 202",
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home<h1>')
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-        return HttpResponse('<h1>Blog About<h1>')
-
-
+    return render(request, "blog/about.html", {"title": "About"})
